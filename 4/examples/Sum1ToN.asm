e7ea734e581d7887140c1b7ff7e7a726b805cbb0
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-05-07 20:27:24 +0100

    remove comments from Sum1ToN

--- 4/examples/Sum1ToN.asm
+++ 4/examples/Sum1ToN.asm
@@ -2,25 +2,6 @@
 // Computes RAM[i] = 1 + 2 + 3 + ... + RAM[0]
 // Usage: put a value >= 1 in RAM[0]
 
-// Pseudocode for Sum1ToN
-    // i = 1
-    // sum = 0
-// LOOP:
-    // if (i > RO) goto STOP
-    // sum = sum + i
-    // i = i + 1
-    // goto LOOP
-// STOP:
-    // R1 = SUM
-
-// Notes
-	// In Hack assembly, every operation involving a memory location entails two instructions
-	// The first instruction @addr is used to select a target memory address;
-	// the subsequent instruction specifies what to do with this address
-	// The language features 2 generic instructions: an address instruction (@N)
-	// and a compute instruction (C-instruction)
-	// Each instruction has a symbolic representation, a binary representation and a effect on the computer
-
 // Figure 4.4 in Machine Language chapter
 
     // i= 1
