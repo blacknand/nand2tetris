4c1f48ef075e4e3fd6c643459cf8a4302e7ae51c
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-05-13 19:35:28 +0100

    Initial test of Mult.asm

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,19 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-
-    @R0
-
-    @R1
-    
+// Pseudocode
+// For i in R1
+    // if (counter > R1)
+        // goto STOP
+    // R2 = R1 + R0
+    // counter = counter + 1
+    // if (counter < R1)
+        // GOTO beginning of for loop
 
 (LOOP)
 
-    @R2
+    // If (count > R1)
+    @END
+    R1;JGT
 
+    // Counter for i in R1
+    @count
+    M=M+1
+
+    // R2 = R1 + R2
+    @R2
+    M=R1+R2
+
+    // If (count < R1)
+    @LOOP
+    R1;JLT
 
 (END)
 
     // Terminate program
+    // by intentionally getting stuck in infinite loop
     @END
     0;JMP
