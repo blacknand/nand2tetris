dc10ddeda1a58b6d437b43d50d1b16e2e2cdf964
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-05-30 21:18:38 +0100

    Remove .o assembler

---
+++ 6/assembler/assembler.h
@@ -0,0 +1,27 @@
+#include <string>
+
+class Parser {
+    public:
+        void initializer(std::string inputFile);
+        bool hasMoreLines();
+        void advance();
+        const std::string instructionType();
+        std::string symbol();
+        std::string dest();
+        std::string comp();
+        std::string jump();
+};
+
+class Code {
+    public:
+        std::string dest(std::string command);
+        std::string comp(std::string command);
+        std::string jump(std::string command);
+};
+
+class SymbolTable {
+    public: 
+        void addEntry(std::string symbol, int address);
+        bool contains(std::string);
+        int getAddress(std::string);
+};
