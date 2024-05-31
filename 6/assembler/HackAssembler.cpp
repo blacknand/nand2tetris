ef86616a3f60ecf1fa5a755cc9a09cacbfd3fc63
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-05-31 14:17:08 +0100

    Add getter + setter

--- 6/assembler/HackAssembler.cpp
+++ 6/assembler/HackAssembler.cpp
@@ -1,25 +1,46 @@
+// Converts hack assembly language into binary
+
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <string>
-#include "code.h"
-#include "parser.h"
-#include "symbolTable.h"
+#include <vector>
+#include "HackAssembler.hpp"
 
 int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout << "Usage: ./HackAssembler prog.asm" << std::endl;
+        return 1;
+    }
+
     Parser parser;
+    parser.setLineCount(0);
+    parser.setCurrentLine(0);
+
     parser.initializer(argv[1]);
+
     return 0;
 }
 
-void Parser::initializer(std::string file) {
+void Parser::initializer(std::string inputFile) {
     std::string line;
-    std::ifstream asmFile(file);
+    std::ifstream asmFile(inputFile);
     if (asmFile.is_open()) {
         while (getline(asmFile, line)) {
             std::cout << line << std::endl;
         }
         asmFile.close();
     } else 
-        std::cout << "Unable to open file" << std::endl;
+        std::cout << "Failed to open " << inputFile << std::endl;
+}
+
+bool Parser::hasMoreLines(int lineCount, int currentLine) {
+    if (currentLine > lineCount)
+        return true;
+    else
+        return false;
+}
+
+void Parser::advance(int currentLine) {
+
 }
--- 6/assembler/HackAssembler.hpp
+++ 6/assembler/HackAssembler.hpp
@@ -1,18 +1,38 @@
 #include <string>
+#include <vector>
 
 #ifndef HACK_ASSEMBLER
 #define HACK_ASSEMBLER
 
 class Parser {
+    private:
+        int lineCount;
+        int currentLine;
+        // std::vector<std::vector<std::string>> god;
     public:
         void initializer(std::string inputFile);
-        bool hasMoreLines();
-        void advance();
+        void close(std::string inputFile);
+        bool hasMoreLines(int lineCount, int currentLine);
+        void advance(int currentLine);
         const std::string instructionType();
         std::string symbol();
         std::string dest();
         std::string comp();
         std::string jump();
+        // Getter
+        void setLineCount(int num) {
+            lineCount = num;
+        }
+        void setCurrentLine(int num) {
+            currentLine = num;
+        }
+        // Setter
+        int getLineCount() {
+            return lineCount;
+        }
+        int getCurrentLine() {
+            return currentLine;
+        }
 };
 
 class Code {
