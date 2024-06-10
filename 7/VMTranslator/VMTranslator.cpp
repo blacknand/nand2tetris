a03e6dcc50ef0a28181e515668908a47d965997d
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-06-10 17:44:43 +0100

    First implementation of Parser::initializer

--- 7/VMTranslator/VMTranslator.cpp
+++ 7/VMTranslator/VMTranslator.cpp
@@ -1,7 +1,28 @@
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 #include "VMTranslator.h"
 
 int main(int argc, char **argv) {
+    Parser parser;
+    std::ifstream inputFile(argv[1]);
+    parser.initializer(inputFile);
+    parser.getFileVect();
     return 1;
+}
+
+void Parser::initializer(std::ifstream &inputFile) {
+    std::string line;
+    std::vector<std::string> lineVect;
+    if (inputFile.is_open()) {
+        while (std::getline(inputFile, line)) {
+            lineVect.clear();
+            lineVect.push_back(line);
+            fileVect.push_back(lineVect);
+        }
+        inputFile.close();
+        return;
+    }
+    std::cout << "Failed to open file" << std::endl;
 }
--- 7/VMTranslator/VMTranslator.h
+++ 7/VMTranslator/VMTranslator.h
@@ -1,16 +1,22 @@
 #include <fstream>
+#include <vector>
 
 #ifndef VM_PARSER
 #define VM_PARSER
 
 class Parser {
+    private:
+        std::vector<std::vector<std::string>> fileVect;
     public:
-        void initializer(std::ifstream inputFile);
+        void initializer(std::ifstream &inputFile);
         bool hasMoreLines();
         void advance();
         const std::string commandType();
         std::string arg1();
         std::string arg2();
+        std::vector<std::vector<std::string>> getFileVect() {
+            return fileVect;
+        }
 };
 
 #endif      // VM_PARSER    
@@ -21,10 +27,10 @@ class Parser {
 
 class CodeWriter {
     public:
-        void initializer(std::ofstream outputFile);
+        void initializer(const std::ofstream &outputFile);
         void writeArithmetic(std::string command);
         void writePushPop(std::string command, std::string segment, int index);
-        void close(std::ofstream outputFile);
+        void close(const std::ofstream &outputFile);
 };
 
 #endif      // CODE_WRITER
