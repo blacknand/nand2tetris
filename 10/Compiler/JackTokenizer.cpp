b7dd3e7a5fd3db32ed945fddb8ebc4e1e1ddf448
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-06-28 14:22:30 +0100

    initial implementation of JackTokenizer::initialise()

--- 10/Compiler/JackTokenizer.cpp
+++ 10/Compiler/JackTokenizer.cpp
@@ -2,3 +2,20 @@
 #include <cstring>
 #include <vector>
 #include "JackTokenizer.h"
+
+
+JackTokenizer::initializer(const char *inputFile) {
+    std::string line;
+    std::ifstream jackFile(inputFile);
+    
+    if (jackFile.is_open()) {
+        while (getline(jackFile, line)) {
+            std::vector<std::string> singleLine;
+            singleLine.push_back(line);
+            lineVect.push_back(singleLine);
+        }
+        jackFile.close();
+    }
+}
+
+
--- 10/Compiler/JackTokenizer.h
+++ 10/Compiler/JackTokenizer.h
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <string>
 
-#ifndef INITIALIZER
-#define INITIALIZER
+#ifndef JACK_TOKENIZER
+#define JACK_TOKENIZER
 
-class Initializer {
+class JackTokenizer {
+    private:
+        std::vector<std::vector<std::string>> lineVect;
     public:
-        void initializer(const char *inputFile);
+        void initializer(std::string inputFile);
         bool hasMoreTokens();
         void advance();
-        const std::string &tokenType() const;
-        const std::string &keyWord() const;
+        const std::string &tokenType();
+        const std::string &keyWord();
         char symbol();
         std::string identifier();
         int intVal();
         std::string stringVal();
 }
 
-#endif      // INITIALIZER
+#endif      // JACK_TOKENIZER
